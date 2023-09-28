634445ca3de63b149a15d042f95dd1d8745ac55c
Cesar Nartam <213473@ids.upchiapas.edu.mx>
2023-09-28 11:29:55 -0600

    player model updated

--- models/player.go
+++ models/player.go
@@ -1,13 +1,44 @@
 package models
 
-import "github.com/faiface/pixel"
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+)
 
 type Player struct {
-	Body pixel.Rect
+	Body   pixel.Rect
+	Speed  float64
+	MoveCh chan float64
+	Score  int
 }
 
-func NewPlayer(body pixel.Rect) *Player {
+func NewPlayer(body pixel.Rect, speed float64) *Player {
 	return &Player{
-		Body: body,
+		Body:   body,
+		Speed:  speed,
+		MoveCh: make(chan float64),
+		Score:  0,
 	}
+}
+
+func (p *Player) Move(win *pixelgl.Window, gameOver bool, windowHeight float64, playerNumber int) {
+	for !gameOver {
+		if win.Pressed(pixelgl.KeyW) && p.Body.Max.Y < windowHeight && playerNumber == 1 {
+			p.MoveCh <- p.Speed
+		}
+		if win.Pressed(pixelgl.KeyS) && p.Body.Min.Y > 0 && playerNumber == 1 {
+			p.MoveCh <- -p.Speed
+		}
+
+		if win.Pressed(pixelgl.KeyUp) && p.Body.Max.Y < windowHeight && playerNumber == 2 {
+			p.MoveCh <- p.Speed
+		}
+		if win.Pressed(pixelgl.KeyDown) && p.Body.Min.Y > 0 && playerNumber == 2 {
+			p.MoveCh <- -p.Speed
+		}
+	}
+}
+
+func (p *Player) UpdateScore(score int) {
+	p.Score = score
 }
